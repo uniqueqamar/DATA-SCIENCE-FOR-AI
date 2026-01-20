892953cab99b77ee5c74974870044c892e68ccaf
qamareen <qirat9923@gmail.com>
2026-01-21 01:47:46 +0530

    Add files via upload

--- 37.py
+++ 37.py
@@ -1,4 +1,14 @@
 import matplotlib.pyplot as plt   
-data =[1,2,3,4,5,1,1,1,2,2,3,4,3,2,1,2,3,]
-plt.hist(data,bins = 5, color ="green",edgecolor= "black")
+#data =[1,2,3,4,5,1,1,1,2,2,3,4,3,2,1,2,3,]
+#plt.hist(data,bins = 5, color ="green",edgecolor= "black")
+#plt.show()
+
+
+x=[1,2,3,4]
+y=[22,25,45,23]
+plt.scatter(x,y,color="red")
+plt.title("Scatter Plot")
+plt.xlabel("X axis")
+plt.ylabel("Y axis")
+plt.legend("Dataset1")
 plt.show()
