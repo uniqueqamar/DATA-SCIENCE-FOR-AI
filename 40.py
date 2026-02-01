12b7d3728e35b599f180bf4b7bf5ce5ab269d4c2
qamareen <qirat9923@gmail.com>
2026-02-01 18:20:59 +0530

    Add files via upload

---
+++ 40.py
@@ -0,0 +1,20 @@
+
+# a = int(input())
+# b = int(input())
+# print(a+b)
+# print(a-b)
+# print(a*b)
+
+# a = int(input())
+# b = int(input())
+# print(a//b)
+# print(a/b)
+
+# 
+# n = int(input())
+# for ( int i=0,i<n,i++){
+#     print(i**2)
+# }
+n=int(input())
+for i in range (n):
+    print(i+1, end="")
---
+++ 43.py
@@ -0,0 +1,10 @@
+from itertools import product
+
+sample_space = list(range(1,7))
+
+
+even_nums =[2,4,6]
+
+P_even = len(even_nums)/len(sample_space)
+
+print(P_even)
